131f2b9d5da68285eec3b9fc83a3707eb40c1397
Julian <54598714+akaJuliaan@users.noreply.github.com>
2023-08-06 22:16:59 +0200

    return microsoft display name

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -25,46 +25,49 @@ public class UserController : ControllerBase
     [HttpGet("login")]
     public async Task<IActionResult> LoginByCode(string code)
     {
-       /* var scopes = new[] { "User.Read" };
-        var tenantId = _configuration.GetValue<string>("AzureAd:TenantId");
-        var clientId = _configuration.GetValue<string>("AzureAd:ClientId");
-        var clientSecret = _configuration.GetSection("AzureAd:ClientCredentials").GetChildren().First().GetValue<string>("ClientSecret");
+        var scopes = new[] { "User.Read" };
+        var tenantId = _configuration["AzureAd:TenantId"];
+        var clientId = _configuration["AzureAd:ClientId"];
+        var clientSecret = _configuration.GetSection("AzureAd:ClientCredentials").GetChildren().First()["ClientSecret"];
 
         var options = new AuthorizationCodeCredentialOptions
         {
             AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
-            RedirectUri = new Uri(""),
+            RedirectUri = new Uri("https://localhost:7061/api/user/login"),
         };
 
         var authCodeCredential = new AuthorizationCodeCredential(tenantId, clientId, clientSecret, code, options);
-        var graphClient = new GraphServiceClient(authCodeCredential, scopes);*/
+        var graphClient = new GraphServiceClient(authCodeCredential, scopes);
 
-       var issuer = _configuration["Jwt:Issuer"];
-       var audience = _configuration["Jwt:Audience"];
-       var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!);
+        var me = await graphClient.Me.GetAsync();
+        if (me is null) return BadRequest("Graph Api Me.Read refused!");
 
-       var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+        var issuer = _configuration["Jwt:Issuer"]; 
+        var audience = _configuration["Jwt:Audience"];
+        var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!);
 
-       var subject = new ClaimsIdentity(new[]
-       {
-           new Claim(JwtRegisteredClaimNames.Sub, "Julian"),
-           new Claim(JwtRegisteredClaimNames.Email, "info@versahl.at"),
-       });
+        var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+        
+        var subject = new ClaimsIdentity(new[]
+        {
+           new Claim(JwtRegisteredClaimNames.Name, me.DisplayName ?? ""),
+           new Claim(JwtRegisteredClaimNames.GivenName, me.GivenName ?? "")
+        });
 
-       var expires = DateTime.UtcNow.AddMinutes(10);
-       var tokenDescriptor = new SecurityTokenDescriptor
-       {
+        var expires = DateTime.UtcNow.AddMinutes(10);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
            Subject = subject,
            Expires = expires,
            Issuer = issuer,
            Audience = audience,
            SigningCredentials = signingCredentials
-       };
+        };
 
-       var tokenHandler = new JwtSecurityTokenHandler();
-       var token = tokenHandler.CreateToken(tokenDescriptor);
-       var jwtToken = tokenHandler.WriteToken(token);
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var jwtToken = tokenHandler.WriteToken(token);
 
-       return Ok(jwtToken);
+        return Ok(jwtToken);
     }
 }
