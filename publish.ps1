e4c4cc7e1ba537d556159f2d16f83983cfa8721a
Dipl-IIS <julian.grill@eco-landeck.at>
2023-08-11 20:48:44 +0200

    create comments

--- publish.ps1
+++ publish.ps1
@@ -1,26 +1,26 @@
 Import-Module IISAdministration
 
-
 if (git status --porcelain) {
 
+	# pull changes
     git pull
+	
+	# restore nuget packeges
     dotnet restore
 	
 	# run publish
 	$result = dotnet publish -c Release *>&1
 
-	# Evaluate success/failure
+	# evaluate if build was successful
 	if($LASTEXITCODE -eq 0)
 	{		
 		# stop api and wait until it is stopped
 		Stop-WebSite 'Api'
 		$currentRetry = 0;
 		$success = $false;
-		do{
+		do {
 			$status = Get-WebsiteState -Name "Api"
 			if ($status.Value -eq "Stopped") {
-					Write-Host "API STOPPED!"
-					Write-Host $status.Value
 					$success = $true;
 					Copy-Item -Path bin/Release/net7.0/publish/* -Force -Destination C:/inetpub/wwwroot
 					Start-WebSite 'Api'
