41167f0c9774592da0c903663a77d28f16747beb
akaJuliaan <info@versahl.at>
2023-10-05 21:32:21 +0200

    create FindAllLogs endpoint

---
+++ Controllers/LogsController.cs
@@ -0,0 +1,31 @@
+using Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers;
+
+/// <inheritdoc />
+[Route("api/[controller]")]
+[ApiController]
+public class LogsController : ControllerBase
+{
+    private readonly ClassInsightsContext _context;
+
+    /// <inheritdoc />
+    public LogsController(ClassInsightsContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    ///     Find all Logs
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> GetLogs()
+    {
+        return Ok(await _context.TabLogs.ToListAsync());
+    }
+}
